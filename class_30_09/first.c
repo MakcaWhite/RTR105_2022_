4a090cc0ae239ff775182a32abd2f0468adceeb4
MakcaWhite <m.javorskis@gmail.com>
2022-09-30 11:33:15 +0300

    class_2022-09-30

---
+++ class_30_09/first.c
@@ -0,0 +1,17 @@
+// komentara viena rinda - // tiek apzimets tikai komentara sakums 
+// komentara cita rinda 
+/*
+komentara paragrafs
+izmanto, piemeram , lai komentetu koda fragmentus
+*/
+int main()
+{
+
+//if()
+//{
+//
+//}
+return 0;
+
+//return 100;
+}
